78757ed5385456088aaf7e615cbb54e0a35378c1
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-03-26 09:43:52 +0100

    commit vor grundsanierung von 'class_details.py'

--- DnD_API/deep_datas/class_details.py
+++ DnD_API/deep_datas/class_details.py
@@ -2,39 +2,57 @@
 Basis classe für alle 12 dnd-Klassen
 stellt die basis struktur für die sub-classen bereit
 
+programm ablauf:
+bekommt ein class_dict mit allen base-class informationen.
+Aus diesem dict werden drei urls gezogen:
+- spells
+- levels
+- subclasses
+
+diese urls geben ein json zurück, aus dem tiefere details zu spells, levels und subclasses gezogen werden.
+diese details werden auch über eine url gezogen.
+
+
 hat funktionen:
 spells() X
-levels() X
-subclases() X
+load_levels() X
+load_subclasses() X
 
 funktionen haben grobe grundstruktur
 geben dict oder None zurück
 
 weitere funktionen:
-all feature descriptions
-all spell descriptions
+all feature details
+all spell details
+all subclass details
 
 warlok braucht z.B noch die extra funktionen für eldrigde Invocations und pact boons
 """
-from abc import ABC
 from DnD_API.dnd_details_fetcher import DnDDetailsFetcher
+from DnD_API.progress_tracker import ProgressTracker
 
 BASE_URL = "https://www.dnd5eapi.co"
 
-class ClassDetails(ABC):
+class ClassDetails:
     def __init__(self, data: dict, class_name: str):
         """
         data: the class_dictionary
+        data wird mit: class_dictionary[i] eingegeben.
         class_name: überprüfungsname für daten
         """
         try:
-            self.data = data # class dictionary
+            self.data = data # dictionary mit basis infos für nur eine einzige klasse
             self.d_fetcher = DnDDetailsFetcher()
 
             # hier wird dann beim instanzieren der class schon ein api zugriff gemacht
-            self.spells = self.spells()
-            self.levels = self.levels()
-            self.subclasses = self.subclasses()
+            if 'spells' not in self.data.keys():
+                self.spells = None
+            else:
+                self.spells = self.load_spells()
+
+
+            self.levels = self.load_levels()
+            self.subclasses = self.load_subclasses()
 
             self.class_name = self.data['name']
 
@@ -44,161 +62,176 @@ class ClassDetails(ABC):
             print(f"Error: {e}")
 
 
-    def spells(self):
+    def load_spells(self):
         spells_url = f"{BASE_URL}{self.data['spells']}"
         # bekommt dann ein dict mit allen spells
         # muss dann für jeden spell eine abfrage für die descirption machen
         self.d_fetcher.detail_url = spells_url
 
+        print(f"laoding base spell data")
         spells_data = self.d_fetcher.load_data()
 
         return spells_data
 
-    def levels(self):
+    def load_levels(self):
         levels_url = f"{BASE_URL}{self.data['class_levels']}"
         # bekommt ein dict mit allen level resources
         self.d_fetcher.detail_url = levels_url
 
+        print(f"laoding base level data")
         levels_data = self.d_fetcher.load_data()
 
         return levels_data
 
         # muss dan noch eine abfrage für die description für alle features machen
 
-    def subclasses(self):
+    def load_subclasses(self):
         subclasses_urls = [f"{BASE_URL}{entry['url']}" for entry in self.data['subclasses']]
-        # bekommt eine liste mit allen urls für subclases (momentan 1)
+        # bekommt eine liste mit allen urls für subclaces (momentan 1)
         # erhält nach abfrage ein dict mit subclass infos
         # wenn subclas spells enthält, dann spell description
         # subclass feature description abfragen
 
         all_subclass_data = []
 
-        for subclass_url in subclasses_urls:
+        print(f"loading base subclass data:")
+        for i, subclass_url in enumerate(subclasses_urls):
+            print(f"loading subclass-{i}")
             self.d_fetcher.detail_url = subclass_url
             subclass_data = self.d_fetcher.load_data()
             all_subclass_data.append(subclass_data)
 
         return all_subclass_data
 
-    def add_spell_details(self):
-        # Durchlaufe alle Zauber-Einträge
+    def spell_details(self):
         enriched_spells = []
+        tracker = ProgressTracker(self.spells["count"], task_name="spell_details")
 
-        for spell in self.spells["results"]:
+        for i, spell in enumerate(self.spells["results"]):
             spell_url = f"{BASE_URL}{spell['url']}"
-
             self.d_fetcher.detail_url = spell_url
-            spell_details = self.d_fetcher.load_data()
+            spell_details_data = self.d_fetcher.load_data()
 
             enriched_spell = {
                 "index": spell["index"],
                 "name": spell["name"],
                 "level": spell["level"],
-                "details": spell_details
+                "details": spell_details_data
             }
             enriched_spells.append(enriched_spell)
 
+            tracker.update(message=f"{i + 1}/{self.spells['count']}")
+
+        tracker.done()
         return enriched_spells
 
-    def add_level_details(self):
-        # durchlaufe alle features für jedes level
+    def level_details(self):
+        tracker = ProgressTracker(len(self.levels), task_name="level_details")
 
-        for level in self.levels:
-
+        for i, level in enumerate(self.levels):
             enriched_features = []
 
-            for feature in level['features']:
+            for j, feature in enumerate(level['features']):
                 feature_url = f"{BASE_URL}{feature['url']}"
-
                 self.d_fetcher.detail_url = feature_url
-                feature_details = self.d_fetcher.load_data()
+                feature_details_data = self.d_fetcher.load_data()
 
                 enriched_features.append({
                     'index': feature['index'],
                     'name': feature['name'],
-                    'details': feature_details
+                    'details': feature_details_data
                 })
 
-            # alte features mit url werden mit features mit details ersetzt
             level['features'] = enriched_features
+            tracker.update(message=f"{i + 1}/{len(self.levels)}")
 
+        tracker.done()
 
-    def add_subclass_details(self):
-        # durchlaufe alle subclasses und erhalte die informationen für die features und zauber der subclass
-
-        # sublcass_details enthalten:
-        # subclass
-        # spells
-        # - spells descriptions
-        # - levels
-        # -- feature-descriptions
-
+    def subclass_details(self):
         enrichted_subclass_spells = []
+        tracker_subclasses = ProgressTracker(len(self.subclasses), task_name="subclass_details")
 
-        for subclass in self.subclasses:
+        for i, subclass in enumerate(self.subclasses):
+            if not subclass:
+                print(f"Warning: subclass {i} is None – skipping.")
+                continue
 
-            # erhält alle spell details
-            spells = subclass['spells']
+            # wenn keine zauber in der subclasse sind
+            if 'spells' not in subclass.keys():
+                return None
 
-            for spell in spells:
-                spell_details_url = f"{BASE_URL}{spell['spell']['url']}"
+            else:
+                spells = subclass['spells']
+                tracker_spells = ProgressTracker(len(spells), task_name=f"subclass_{i+1}_spells")
 
-                self.d_fetcher.detail_url = spell_details_url
-                spell_details = self.d_fetcher.load_data()
+                for j, spell in enumerate(spells):
+                    spell_details_url = f"{BASE_URL}{spell['spell']['url']}"
+                    self.d_fetcher.detail_url = spell_details_url
+                    spell_details_data = self.d_fetcher.load_data()
 
-                enrichted_subclass_spells.append({
-                    'prerequisites': spell['spells']['prerequisites'],
-                    'index': spell['spells']['index'],
-                    'name': spell['spells']['name'],
-                    'details': spell_details
-                })
+                    enrichted_subclass_spells.append({
+                        'prerequisites': spell['spells']['prerequisites'],
+                        'index': spell['spells']['index'],
+                        'name': spell['spells']['name'],
+                        'details': spell_details_data
+                    })
 
-            # erhält alle features zur subclass
+                    tracker_spells.update(message=f"{j + 1}/{len(spells)}")
+
+                tracker_spells.done()
+
             enriched_subclasses = []
-
             subclass_levels_url = f"{BASE_URL}{subclass['subclass_levels']}"
-
             self.d_fetcher.detail_url = subclass_levels_url
-            subclass_levels = self.d_fetcher.load_data()
+            subclass_levels_data = self.d_fetcher.load_data()
 
             enriched_subclass_levels = []
 
-            for subclass_level in subclass_levels:
+            for k, subclass_level in enumerate(subclass_levels_data):
                 subclass_level_url = f"{BASE_URL}{subclass['subclass_levels']}"
-
                 self.d_fetcher.detail_url = subclass_level_url
                 subclass_level = self.d_fetcher.load_data()
 
-                # erhält alle feature details
                 enriched_features = []
 
-                for feature in subclass_levels['features']:
+                for l, feature in enumerate(subclass_levels_data['features']):
                     subclass_feature_url = f"{BASE_URL}{subclass['subclass_levels']}"
-
                     self.d_fetcher.detail_url = subclass_feature_url
-                    feature_details = self.d_fetcher.load_data()
+                    feature_details_data = self.d_fetcher.load_data()
 
-                    # fügt feature detaisl aus api request in jedes feature hinzu
                     enriched_features.append({
                         'index': feature['index'],
                         'name': feature['name'],
-                        'details': feature_details
+                        'details': feature_details_data
                     })
 
-                # fügt alle level freatures mit beschreibungen hinzu
                 enriched_subclass_levels.append({
                     'level': subclass_level['level'],
                     'features': enriched_features,
                 })
 
-            # fügt die subclass mit allen spells und spellbeschreibungen und features und feature beschreibungen hinzu
             enriched_subclasses.append({
                 'index': subclass['index'],
                 'class': subclass['class'],
                 'name': subclass['name'],
                 'subclass_flavor': subclass['sublcass_flavor'],
                 'description': subclass['desc'],
-                'spells': enrichted_subclass_spells, # hier müssen dann noch die enrichted spells rein
-                'subclass_levels': subclass_levels
+                'spells': enrichted_subclass_spells,
+                'subclass_levels': subclass_levels_data
             })
+
+            tracker_subclasses.update(message=f"{i + 1}/{len(self.subclasses)}")
+
+        tracker_subclasses.done()
+
+
+
+
+    def all_details(self) -> tuple:
+        if self.spells:
+            self.spell_details()
+
+        self.level_details()
+        self.subclass_details()
+
+        return self.spells, self.levels, self.subclasses
