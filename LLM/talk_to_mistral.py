48f54dce89fcddf5ad8169de6dd9eeade3c27a81
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-03-27 11:52:10 +0100

    __init__ talk_to_mistral.py Klasse, die für die Kommunikation mit Mistral zuständig ist.

---
+++ LLM/talk_to_mistral.py
@@ -0,0 +1,16 @@
+from mistralai import Mistral
+
+
+class TalkToMistral:
+
+    api_key = "TYPE YOUR API KEY"
+    model = "mistral-large-latest"
+
+    client = Mistral(api_key=api_key)
+
+    chat_response = client.chat.complete(
+        model=model,
+        messages=[{"role": "user", "content": "What is the best French cheese?"}]
+    )
+
+    print(chat_response.choices[0].message.content)
