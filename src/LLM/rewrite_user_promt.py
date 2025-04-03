c9ecf47edbb57ddc009c8f8677ecebfc412189f4
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-04-03 12:54:13 +0200

    analyse user prompt und rewrite user prompt in eine klasse vereinigt

--- src/LLM/rewrite_user_promt.py
+++ src/LLM/rewrite_user_promt.py
@@ -1,21 +1,57 @@
-"""
-schreibt den user promt um.
-von ‘dunkler Magier, der tote beschwört und feuer Zauber beherrscht’
- wird: 'dunkler {Klassen_name}, der tote beschwört und feuer Zauber beherrscht.
+"""Gegeben ist der folgende Nutzer-Prompt:
+{user_promt}
+
+Finde die zu diesem Prompt am besten passenden Klassen aus folgender 'class_name' Liste:
+- barbarian
+- bard
+- cleric
+- druid
+- fighter
+- monk
+- paladin
+- ranger
+- rogue
+- sorcerer
+- warlock
+- wizard
+
+Gib als Ergebnis ein JSON mit folgendem Format zurück:
+{
+"matched_classes": [best_class, second_best_class, thrid_best_class],
+"keywords": [keywords aus user_promt, die den character, seine Fähigkeiten und Eingeschaften beschreiben],
+"rewritten_prompt_template": [umgeschriebene user_prompt mit best_class, umgeschriebener user_promt mit second_best_clas, ...]
+}
+
+Beispiel:
+user_promt: "dunkler Magier, der tote beschwört und feuer Zauber beherrscht"
+JSON rückgabe:
+{
+"matched_classes": ["wizard", "warlock", "cleric"],
+"keywords": ["dunkel", "beschwörung", "feuer", "tote", "zauber"],
+"rewritten_prompt_template": ["dunkler wizard, der tote beschwört und Feuerzauber beherrscht", "dunkler warlock, der tote beschwört und Feuerzauber beherrscht", "dunkler cleric', der tote beschwört und Feuerzauber beherrscht"]
+}
+
 """
 from src.LLM.talk_to_mistral import TalkToMistral
+from src.handle_data.CRUD import CRUD
+
+prompt_data_path = "../../static_dnd_data/system_messages/system_prompts_for_analysing.json"
 
 class RewriteUserPromt(TalkToMistral):
-    def __init__(self, class_name: str):
+    def __init__(self, user_prompt, prompt_key: str= "prompt_alpha_1"):
+        """
+        crud: the class, wich gets the prompt from the prompt file
+        prompt_key: the key to geht the correct prompt from the prompt dict
+        """
         super().__init__()
-        self.class_name = class_name
+        self.crud = CRUD(prompt_data_path)
+        self.prompt_key = prompt_key
+        self._user_prompt = user_prompt
 
-    def rewrite(self, user_promt):
+    def rewrite(self):
         """fragt das LLM, den User-Promt umzuschreiben"""
-        stippted_user_prompt = user_promt.strip() # definiert den user_promt
-        request = (f"Schreibe den folgenden Satz um, wobei du das zentrale Wesen durch "
-                 f"'{self.class_name}' ersetzt: '''{stippted_user_prompt}'''."
-                   f"Gebe nur den umgeschriebenen Satz zurück. Füge keine erklärungen hinzu.")
+        #user_prompt = user_promt.strip() # definiert den user_promt
+        request = self.generate_request_prompt()
         self.ask(request) # gibt Mistral den Auftrag den user_promt umzuschreiben
 
         return self.response() # gibt umgeschriebenen user_promt zurück
@@ -31,13 +67,33 @@ class RewriteUserPromt(TalkToMistral):
         self._user_prompt = user_promt
 
 
+    def generate_request_prompt(self) -> str:
+        """generates the prompt for the request, out of the given system_prompt and the user_prompt"""
+        request: str = self.crud.data[self.prompt_key]
+
+        request_prompt = request.replace("{PLACEHOLDER}", self._user_prompt)
+
+        return request_prompt
+
+
 
 
 if __name__ == "__main__":
-    rewrite = RewriteUserPromt('fighter')
+    crud = CRUD()
 
+    prompt_key = "prompt_alpha_3"
     user_promt = "dunkler Magier, der tote beschwört und feuer Zauber beherrscht"
 
-    rewritten_promt = rewrite.rewrite(user_promt)
+    data_strukture = [
+        {
 
+        }
+    ]
+
+    rewrite = RewriteUserPromt(user_promt, prompt_key=prompt_key)
+
+    rewritten_promt = rewrite.rewrite()
+
+
+    print(type(rewritten_promt))
     print(rewritten_promt)
