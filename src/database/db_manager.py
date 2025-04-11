129cb2e43d70552251a2085b397786c51000a80c
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-04-11 14:58:57 +0200

    debug_logg für jede methode in db_manager.py eingfügt

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -1,6 +1,6 @@
 from sqlalchemy import func, select, delete
 from sqlalchemy.orm import sessionmaker
-from models import CharIdea, Character, RewrittenPrompts, KeyDescription, DescriptionToIdea, Classes, BestChar
+from src.database.models import CharIdea, Character, RewrittenPrompts, KeyDescription, DescriptionToIdea, Classes, BestChar
 from typing import List, Dict
 from src.debug.debug_log import DebugLog
 
@@ -21,8 +21,9 @@ class DatabaseManager:
 	"""
 	def __init__(self, engine):
 		self.Session = sessionmaker(bind=engine)
-
-	def save_user_prompt(self, prompt: str, analysed_dict: dict[list[str]]):
+	
+	@DebugLog.debug_log
+	def save_user_prompt(self, prompt: str, analysed_dict: dict[str, list[str]]):
 		"""
 		speichert alle daten aus der Analyse des user_prompts
 		"""
@@ -30,231 +31,174 @@ class DatabaseManager:
 		possible_classes, key_descriptions, rewritten_prompts = self.extract_analysed_data(analysed_dict)
 
 		session = self.Session()
-		try:
-			# user_prompt abspeichern
-			self.save_char_idea(prompt, session)
+		# user_prompt abspeichern
+		self._save_char_idea(prompt, session)
 
-			# gibt die nueste / höchste idea_id heraus
-			stmt_new_idea = select(func.max(CharIdea.idea_id))
-			new_idea_id = session.scalars(stmt_new_idea).first()
+		# gibt die nueste / höchste idea_id heraus
+		stmt_new_idea = select(func.max(CharIdea.idea_id))
+		new_idea_id = session.scalars(stmt_new_idea).first()
 
-			# possible_classes abspeichern
-			self.save_classes(new_idea_id, session)
+		# possible_classes abspeichern
+		self._save_classes(new_idea_id, session)
 
-			# key_descriptions abspeichern
-			self.save_key_descriptions(key_descriptions, new_idea_id, session)
+		# key_descriptions abspeichern
+		self._save_key_descriptions(key_descriptions, new_idea_id, session)
 
-			# umgeschriebene user_prompts abspeichern
-			self.save_rewritten_prompts(new_idea_id, rewritten_prompts, session)
+		# umgeschriebene user_prompts abspeichern
+		self._save_rewritten_prompts(new_idea_id, rewritten_prompts, session)
 
-			session.flush()
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in save_useer_prompt, db_manager.py")
-			session.rollback()
-			raise e
-		finally:
-			session.close()
-
-	def save_char_idea(self, prompt, session):
+		session.flush()
+		session.close()
+	
+	@DebugLog.debug_log
+	def _save_char_idea(self, prompt, session):
 		"""Saves user_prompt in db"""
-		try:
-			char_idea = CharIdea(user_prompt=prompt)
-			session.add(char_idea)
-			session.flush()
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in save_char_idea, db_manager.py")
-			raise e
-
-	def save_rewritten_prompts(self, new_idea_id, rewritten_prompts, session):
+		char_idea = CharIdea(user_prompt=prompt)
+		session.add(char_idea)
+		session.flush()
+	
+	@DebugLog.debug_log
+	def _save_rewritten_prompts(self, new_idea_id, rewritten_prompts, session):
 		"""saves the three rewritten user_prompts in db"""
-		try:
-			for i, r_prompt in enumerate(rewritten_prompts):
-				rewritten_promp = RewrittenPrompts(idea_id=new_idea_id, rewritten_promp=r_prompt)
-				session.add(rewritten_promp)
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in save_rewritten_prompts, db_manager.py")
-			raise e
-
-	def save_key_descriptions(self, key_descriptions, new_idea_id, session):
+		for i, r_prompt in enumerate(rewritten_prompts):
+			rewritten_promp = RewrittenPrompts(idea_id=new_idea_id, rewritten_promp=r_prompt)
+			session.add(rewritten_promp)
+	
+	@DebugLog.debug_log
+	def _save_key_descriptions(self, key_descriptions, new_idea_id, session):
 		"""saves the key_descriptions from user_prompt in db"""
-		try:
-			for i, key_description in enumerate(key_descriptions):
-				stmt_all = select(KeyDescription.description)
-				all_key_descriptions = session.scalars(stmt_all).all()
-				if not key_description in all_key_descriptions:
-					description = KeyDescription(description=key_description)
-					session.add(description)
-					stmt_max = select(func.max(KeyDescription.description_id))
-					description_id = session.scalars(stmt_max).first()
-				else:
-					description_id = session.scalars(
-						select(KeyDescription.description_id).where(KeyDescription.description == key_description)).first()
+		for i, key_description in enumerate(key_descriptions):
+			stmt_all = select(KeyDescription.description)
+			all_key_descriptions = session.scalars(stmt_all).all()
+			if not key_description in all_key_descriptions:
+				description = KeyDescription(description=key_description)
+				session.add(description)
+				stmt_max = select(func.max(KeyDescription.description_id))
+				description_id = session.scalars(stmt_max).first()
+			else:
+				description_id = session.scalars(
+					select(KeyDescription.description_id).where(KeyDescription.description == key_description)).first()
 
-				description_to_idea = DescriptionToIdea(idea_id=new_idea_id, description_id=description_id)
-				session.add(description_to_idea)
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in save_key_descriptions, db_manager.py")
-			raise e
-
-	def save_classes(self, new_idea_id, session):
+			description_to_idea = DescriptionToIdea(idea_id=new_idea_id, description_id=description_id)
+			session.add(description_to_idea)
+	
+	@DebugLog.debug_log
+	def _save_classes(self, new_idea_id, session):
 		"""saves the three best fitting dnd-classes for user_prompt in db"""
-		try:
-			possible_classes = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'rogue',
-								'sorcerer', 'warlock', 'wizard']
-			dnd_classes = Classes(idea_id=new_idea_id,
-								  barbarian='barbarian' in possible_classes,
-								  bard='bard' in possible_classes,
-								  cleric='cleric' in possible_classes,
-								  druid='druid' in possible_classes,
-								  fighter='fighter' in possible_classes,
-								  monk='monk' in possible_classes,
-								  paladin='paladin' in possible_classes,
-								  ranger='ranger' in possible_classes,
-								  rogue='rogue' in possible_classes,
-								  sorcerer='sorcerer' in possible_classes,
-								  warlock='warlock' in possible_classes,
-								  wizard='wizard' in possible_classes)
-			session.add(dnd_classes)
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in save_classes, db_manager.py")
-			raise e
-
+		possible_classes = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'rogue',
+							'sorcerer', 'warlock', 'wizard']
+		dnd_classes = Classes(idea_id=new_idea_id,
+							  barbarian='barbarian' in possible_classes,
+							  bard='bard' in possible_classes,
+							  cleric='cleric' in possible_classes,
+							  druid='druid' in possible_classes,
+							  fighter='fighter' in possible_classes,
+							  monk='monk' in possible_classes,
+							  paladin='paladin' in possible_classes,
+							  ranger='ranger' in possible_classes,
+							  rogue='rogue' in possible_classes,
+							  sorcerer='sorcerer' in possible_classes,
+							  warlock='warlock' in possible_classes,
+							  wizard='wizard' in possible_classes)
+		session.add(dnd_classes)
+	
+	@DebugLog.debug_log
 	def extract_analysed_data(self, analysed_dict: dict[list[str]]) -> tuple:
 		"""
 		extrahiert alle daten aus dem dictionary und gibt sie als tuple zurück
 		"""
-		try:
-			classes: list[str] = analysed_dict["matched_classes"]
-			key_words: list[str] = analysed_dict["keywords"]
-			rewritten_prompts: list[str] = analysed_dict["rewritten_prompt_template"]
-			return classes, key_words, rewritten_prompts
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in extract_analysed_data, db_manager.py")
-			raise e
-
+		classes: list[str] = analysed_dict["matched_classes"]
+		key_words: list[str] = analysed_dict["keywords"]
+		rewritten_prompts: list[str] = analysed_dict["rewritten_prompt_template"]
+		return classes, key_words, rewritten_prompts
+	
+	@DebugLog.debug_log
 	def load_all_char_ideas(self):
 		"""lädt alle char_ideen und gibt sie als dict mit key=id und value=idea zurück"""
 		session = self.Session()
 
-		try:
-			stmt = select(CharIdea)
-			result = session.execute(stmt).all()
+		stmt = select(CharIdea)
+		result = session.execute(stmt).all()
 
-			if not result:
-				return {}
+		if not result:
+			return {}
 
-			return result
-
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in load_all_char_ideas, db_manager.py")
-			session.rollback()
-			raise e
-		finally:
-			session.close()
-
+		return result
+	
+	@DebugLog.debug_log
 	def load_character_data(self, idea_id):
 		"""loads all data form the analysed user_prompt for creating a system prompt for character creation"""
-		#TODO: Error handling einbauen
-
 		session = self.Session()
-		try:
-			classes_for_idea =  self.load_classes_for_idea(idea_id, session)
+		classes_for_idea =  self._load_classes_for_idea(idea_id, session)
 
-			descriptions_for_idea = self.load_key_description_for_idea(idea_id, session)
+		descriptions_for_idea = self._load_key_description_for_idea(idea_id, session)
 
-			rewritten_prompts = self.load_rewritten_prompts(idea_id, session)
+		rewritten_prompts = self._load_rewritten_prompts(idea_id, session)
 
-			return {
-				'classes': classes_for_idea,
-				'key_descriptions': descriptions_for_idea,
-				'rewritten_prompts': rewritten_prompts
-			}
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in load_character_data, db_manager.py")
-			raise e
-		finally:
-			session.close()
-
-	def load_rewritten_prompts(self, idea_id, session):
+		return {
+			'classes': classes_for_idea,
+			'key_descriptions': descriptions_for_idea,
+			'rewritten_prompts': rewritten_prompts
+		}
+	
+	@DebugLog.debug_log
+	def _load_rewritten_prompts(self, idea_id, session):
 		"""loads all rewritten prompts with the idea_id i"""
-		try:
-			stmt = select(RewrittenPrompts.rewritten_prompt).where(RewrittenPrompts.idea_id == idea_id)
-			result = session.execute(stmt).all()
+		stmt = select(RewrittenPrompts.rewritten_prompt).where(RewrittenPrompts.idea_id == idea_id)
+		result = session.execute(stmt).all()
 
-			if result:
-				return result
+		if result:
+			return result
 
-			return []
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in load_rewritten_prompts, db_manager.py")
-			raise e
-
-	def load_key_description_for_idea(self, idea_id, session):
+		return []
+	
+	@DebugLog.debug_log
+	def _load_key_description_for_idea(self, idea_id, session):
 		"""
 		gibt alle descriptions zurück, die eine key-key paarung mit der idea_id haben
 		"""
-		try:
-			descriptions: list[str] = []
-			stmt = select(DescriptionToIdea.description_id).where(DescriptionToIdea.idea_id == idea_id)
-			result = session.execute(stmt).all()
-			if result:
-				for i, description_id in enumerate(result):
-					description: str = select(KeyDescription.description).where(KeyDescription.description_id == description_id).first()
-					if description:
-						descriptions.append(description)
-			return descriptions
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in load_key_description_for_idea, db_manager.py")
-			raise e
-
-	def load_classes_for_idea(self, idea_id, session):
+		descriptions: list[str] = []
+		stmt = select(DescriptionToIdea.description_id).where(DescriptionToIdea.idea_id == idea_id)
+		result = session.execute(stmt).all()
+		if result:
+			for i, description_id in enumerate(result):
+				description: str = select(KeyDescription.description).where(KeyDescription.description_id == description_id).first()
+				if description:
+					descriptions.append(description)
+		return descriptions
+	
+	@DebugLog.debug_log
+	def _load_classes_for_idea(self, idea_id, session):
 		"""gives back all class_names from Table Classes, wich are True"""
-		try:
-			stmt = select(Classes).where(Classes.idea_id == idea_id)
-			result = session.execute(stmt).first()
-			if result:
-				class_names = [column.name for column in Classes.__table__.columns if getattr(result[0], column.name) is True and column.name != "class_table_id" and column.name != "idea_id"]
-				return class_names
-			return []
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in load_classes_for_idea, db_manager.py")
-			raise e
-
+		stmt = select(Classes).where(Classes.idea_id == idea_id)
+		result = session.execute(stmt).first()
+		if result:
+			class_names = [column.name for column in Classes.__table__.columns if getattr(result[0], column.name) is True and column.name != "class_table_id" and column.name != "idea_id"]
+			return class_names
+		return []
+	
+	@DebugLog.debug_log
 	def save_generated_characters(self, characters: List[Dict]):
+		"speichert die erstellen charactere in der db ab"
 		session = self.Session()
-		try:
-			for character in characters:
-				character_entry = Character(**character)
-				session.add(character_entry)
+		for character in characters:
+			character_entry = Character(**character)
+			session.add(character_entry)
 
-			session.commit()
-
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in save_generated_characters, db_manager.py")
-			session.rollback()
-			raise e
-		finally:
-			session.close()
-
+		session.commit()
+	
+	@DebugLog.debug_log
 	def delete_character_by_idea_id(self, idea_id: int):
 		"""Löscht eine char_idea mit allen colums, die damit verbunden sind"""
 		session = self.Session()
-		try:
-			# list of all models, which containing the idea_id as foregin_key
-			models = [CharIdea, DescriptionToIdea, RewrittenPrompts, Classes, Character, BestChar]
+		# list of all models, which containing the idea_id as foregin_key
+		models = [CharIdea, DescriptionToIdea, RewrittenPrompts, Classes, Character, BestChar]
 
-			# looping thrue all models
-			for i, model in enumerate(models):
+		# looping thrue all models
+		for i, model in enumerate(models):
 
-				# deleting all colums with the idea_id == idea_id
-				stmt = delete(model).where(model.idea_id == idea_id)
-				session.execute(stmt)
+			# deleting all colums with the idea_id == idea_id
+			stmt = delete(model).where(model.idea_id == idea_id)
+			session.execute(stmt)
 
-			session.commit()
-
-		except Exception as e:
-			DebugLog.log_error(e, context="Fehler in delete_character_by_idea_id, db_manager.py")
-			session.rollback()
-			raise e
-		finally:
-			session.close()
+		session.commit()
