fda192e303c77cd28b7c271b9cb6e97cab8189f8
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-04-15 14:28:29 +0200

    indents gefixt. von leertaste zu tabs

--- src/DnD_API/dnd_class_fetcher.py
+++ src/DnD_API/dnd_class_fetcher.py
@@ -2,20 +2,20 @@ from src.DnD_API.dnd_api_base import DnDAPIBase
 from src.handle_data.CRUD import CRUD
 
 class DnDClassFetcher(DnDAPIBase):
-    def __init__(self, url):
-        super().__init__(url)
-        self.crud = CRUD("../../static_dnd_data/all_classes.json")
+	def __init__(self, url):
+		super().__init__(url)
+		self.crud = CRUD("../../static_dnd_data/all_classes.json")
 
-    def load_and_save(self):
-        self.load_data()
-        indicies = [key for key in self.data.keys() if key != 'updated_at']
-        class_data_dict = {i: self.data.get(i) for i in indicies}
+	def load_and_save(self):
+		self.load_data()
+		indicies = [key for key in self.data.keys() if key != 'updated_at']
+		class_data_dict = {i: self.data.get(i) for i in indicies}
 
-        self.save_data(class_data_dict)
-
-    # temporary method
-    def save_data(self, new_data):
-        self.crud.data = new_data
+		self.save_data(class_data_dict)
+	 
+	# temporary method
+	def save_data(self, new_data):
+		self.crud.data = new_data
 
 
 
