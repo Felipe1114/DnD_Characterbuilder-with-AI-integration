5f58d853179984e555a72c6e93156d17acb53f22
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-04-22 12:19:32 +0200

    system_request_builder.py ist fertig
    system prompt-liste wird erstellt.
    returned wird eine liste mit 4 prompts, die über eine for schleife im character_generator(name nicht korreckt) ausgeführt werden können

--- src/LLM/system_request_builder.py
+++ src/LLM/system_request_builder.py
@@ -4,6 +4,7 @@ Sie analysiert den Prompt und kombiniert ihn mit den JSON-Daten.
 Aufgabe:
 erhält Klassen daten und umgeschriebenen User-Promt
 Kombiniert alles zu einem “System Prompt”"""
+
 from src.database.db_manager import DatabaseManager
 from src.handle_data.crud_txt import CrudTxtFiles
 
@@ -17,8 +18,8 @@ class SystemRequestBuilder:
 		self.get_class_data = ...
 		
 	
-	def get_system_message(self):
-		"""gets the System message from the local storage"""
+	def get_system_message_template(self):
+		"""gets the System message-template from the local storage"""
 		system_message = self.crud.data
 		return system_message
 	
@@ -27,44 +28,53 @@ class SystemRequestBuilder:
 		returns a dict with 3 different classes and rewirtten_user_prompts
 		"""
 		char_prompts = self.db.load_character_prompts(self.idea_id)
-		# TODO bereite daten so auf, dass man einfach über sie iterieren kann
-		"""
-		dieses dict :
-		{
-			'classes': classes_for_idea,
-			'key_descriptions': descriptions_for_idea,
-			'rewritten_prompts': rewritten_prompts
+		
+		char_prompt_dict = {
+								"char_1": {
+											"class": char_prompts["classes"][0],
+											"key_descriptions": char_prompts["key_descriptions"],
+											"rewritten_prompt": char_prompts["rewritten_prompt"[0]]
+											},
+								"char_2": {
+											"class": char_prompts["classes"][0],
+											"key_descriptions": char_prompts["key_descriptions"],
+											"rewritten_prompt": char_prompts["rewritten_prompt"[0]]
+											},
+								"char_3": {
+											"class": char_prompts["classes"][1],
+											"key_descriptions": char_prompts["key_descriptions"],
+											"rewritten_prompt": char_prompts["rewritten_prompt"[1]]
+											},
+								"char_4": {
+											"class": char_prompts["classes"][2],
+											"key_descriptions": char_prompts["key_descriptions"],
+											"rewritten_prompt": char_prompts["rewritten_prompt"[2]]
+											}
 		}
 		
-		soll in diese struktur überführt werden:
-		{
-			char_1: {"class": class_1, "key_descriptions": [key_descriptions], "rewritten_prompt": rewritten_prompt},
-			char_2: {"class": class_1, "key_descriptions": [key_descriptions], "rewritten_prompt": rewritten_prompt},
-			char_3: {"class": class_2, "key_descriptions": [key_descriptions], "rewritten_prompt": rewritten_prompt},
-			char_4: {"class": class_3, "key_descriptions": [key_descriptions], "rewritten_prompt": rewritten_prompt}
-			}
-		"""
-		return char_prompts
+		return char_prompt_dict
 	
-	def generate_system_prompt(self):
-		"""puts rewritten_prompt and system_message to system_prompt together"""
-		"""
-		es sollen vier charactere generiert werden.
-		Aus der datenbank kommt ein dict, mit jeweils drei unterschiedlichen classen und rewritten_prompts
+	def generate_system_prompts(self):
+		"""creates a list with four system prompts. Each for a character.
+		Each system prompt contains the system_prompt_template, the rewritten_prompt for each possible class
+		and all necessary data for each class"""
+		system_message_with_char_prompt = []
+		char_prompt_dcit = self.load_char_prompts()
 		
-		es muss eine for-schleife (4 mal) geschrieben werden, die 4 charactere auf basis der classen und rewritten prompts erstellt
-		class_list = [class_1, class_2, class_3]
-		character aufteilung: 2x class_1, 1x class_2, 1x class_3
-			->  system_messages = []
-				for i in range(4):
-					if i <= 1:
-						system_messages.append(generieree system_message für class_list[0])
-					else:
-						system_messages.append(generieree system_message für class_list[i-1])
-					
-		"""
-		system_message = self.get_system_message()
+		for key, data in char_prompt_dcit.items():
+			char_details = f"Erstelle einen {data["rewritten_prompt"]}. Orientiere dich bei dem {data["class"]} an folgenden details: {data["key_descriptions"]}"
+			# for each char a clean system_message_template
+			system_message = self.get_system_message_template()
+			# replaces Placeholder: __RewrittenPrompt__ with char_details
+			system_message = system_message.replace("__RewrittenPrompt__", char_details)
+			
+			system_message_with_char_prompt.append(system_message)
+			
+		return system_message_with_char_prompt
+			
 		
-		#TODO hier muss vieleicht mit "yield" gearbeitet werden, da es ja drei system messages gibt, die ausgegeben werden sollen
+		
+			
+			
 		
 		
