7e400ef01d8676a06cb6522a7a573b8d29055136
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-04-22 12:36:42 +0200

    versucht file zu senden. bisher nur 401 und 422 error bekommen

--- src/LLM/upload_files_übung.py
+++ src/LLM/upload_files_übung.py
@@ -22,4 +22,30 @@ import requests
 
 
 
+url = "https://api.mistral.ai/v1/files"
+
+mein_body = {
+	"id": "497f6eca-6276-4993-bfeb-53cbbbba6f09",
+	"object": "/Users/felipepietzsch/Masterschool/Ohne Titel/DnD_Characterbuilder-with-AI-integration/static_dnd_data/detailed_class_data/cleric",
+	"bytes": 13000,
+	"created_at": 1716963433,
+	"filename": "clericcleric.jsonl",
+	"purpose": "fine-tune",
+	"sample_type": "pretrain",
+	"num_lines": 0,
+	"source": "upload"
+}
+
+headers = {
+    "Authorization": f"Bearer EBBtyAxkHIZOWJcTz3AzsTH0xyDKcDKt", #TODO: API key muss aus .env kommen!!!!!!!
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=mein_body, headers=headers)
+
+if response.status_code == 200:
+    print("Erfolg:", response.json())
+else:
+    print("Fehler:", response.status_code, response.text)
+
 
