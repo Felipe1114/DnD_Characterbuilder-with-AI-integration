0a9ee845f689d49db40260e608dce007f98b3337
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-05-06 09:59:24 +0200

    datenbank wurde erstsellt

---
+++ src/database/db_creator.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.database.models import Base
+
+# Erstelle eine SQLite-Datenbank im angegebenen Pfad
+DATABASE_URL = f"sqlite:///../../data/db/dnd_db.sqlite"
+
+if __name__ == "__main__":
+	
+	engine = create_engine(DATABASE_URL)
+	
+	# Erstelle alle Tabellen in der Datenbank
+	Base.metadata.create_all(engine)
+	
+	# Optional: Erstelle eine Sitzung, um mit der Datenbank zu interagieren
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	
+	# Beispiel: Füge einen Eintrag hinzu (optional)
+	# new_idea = CharIdea(user_prompt="Create a powerful sorcerer")
+	# session.add(new_idea)
+	# session.commit()
+	
+	print("Datenbank und Tabellen wurden erfolgreich erstellt.")
