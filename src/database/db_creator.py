ba038bd24be7cfc942c132ba6c8ede9d61fd7b4c
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-05-26 13:42:07 +0200

    envloader in db_creator.py eingefügt

--- src/database/db_creator.py
+++ src/database/db_creator.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.database.models import Base
+from src.handle_data.env_loader import EnvLoader
 
 # Erstelle eine SQLite-Datenbank im angegebenen Pfad
-DATABASE_URL = "sqlite:///../../data/db/dnd_db.sqlite"
+
+DATABASE_URL = EnvLoader.db_path()
 
 if __name__ == "__main__":
 	
