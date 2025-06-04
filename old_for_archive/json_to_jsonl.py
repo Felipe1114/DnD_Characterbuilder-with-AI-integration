2a32f138d1462f5ce37bd451dceb750d76b1532b
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-06-04 09:43:27 +0200

    unnötige Files in 'archiv' ordner gepackt

--- old_for_archive/json_to_jsonl.py
+++ old_for_archive/json_to_jsonl.py
@@ -4,6 +4,7 @@ converts all four files for a dnd class from json to jsonl
 This is nececary, cause the mistral api only accepts jsonl files in the request body
 """
 import json
+
 class JsonToJsonl:
 	def convert_json_to_jsonl(self, input_data, output_file):
 		with open(output_file, 'w') as jsonl_file:
