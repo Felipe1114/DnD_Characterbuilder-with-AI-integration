d38eb350004462ed822c0140e699d0fc76c78a66
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-06-04 11:02:48 +0200

    überflüssige datein in archiv verschoben oder gelöscht

--- old_for_archive/überprüfungen/analyse_class_name_überprüfung.py
+++ old_for_archive/überprüfungen/analyse_class_name_überprüfung.py
@@ -5,7 +5,7 @@ from src.LLM.analyse_user_promt import AnalyceClassName
 from src.handle_data.crud_json import CrudJsonFiles
 
 def main():
-    crud = CrudJsonFiles("../static_dnd_data/class_keywords.json")
+    crud = CrudJsonFiles("../../static_dnd_data/class_keywords.json")
     class_keywords = crud.data
 
     analyzer = AnalyceClassName(class_keywords=class_keywords)
