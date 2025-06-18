5be6d946af10aed357db1e9779026242591d4402
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-06-18 13:44:07 +0200

    logging for character_generation_endpoints.py ready

--- src/api_endpoints/character_generation_endpoints.py
+++ src/api_endpoints/character_generation_endpoints.py
@@ -10,15 +10,18 @@ GET:
 from fastapi import APIRouter, HTTPException
 from src.llm.character_builder_app import CharacterBuilderApp
 from src.database.db_manager import DatabaseManager
+from src.helper.logger import Logger
+
 
 router = APIRouter()
 db_mngr = DatabaseManager()
-
+logger = Logger("api")
 
 @router.get("/get")
 async def get_characters(user_input: int):
 	"""gets all characters by user_prompt_id"""
 	try:
+		logger.info(f"Execute endpoit: /characters/get?user_input={user_input}")
 		# validates user_input
 		# if not, raise HTTPException with 400 status_code
 		user_prompt_id = validate_input(user_input)
@@ -26,7 +29,7 @@ async def get_characters(user_input: int):
 		# does user_prompt_id exist in db?
 		# if not, raise HTTPException with 404 status_code
 		user_prompt_id = validate_user_prompt_id(user_prompt_id)
-	
+		
 		character_list = db_mngr.load_characters(user_prompt_id)
 		
 		return character_list
@@ -77,43 +80,74 @@ async def generate_characters(user_input: int):
 def validate_input(user_input):
 	"""validates user_input; if user_input can not be transformed into an int, an error is raised"""
 	try:
+		logger.debug(f"user_input:{user_input}, is in valdiation process; checked if it is an int, or str.isdecimal(). If str.isdecimal() -> change type str to int")
+		logger.info(f"Data type of user_input:{user_input}, is in valdiation process...")
+		
 		# user_input is int
 		if isinstance(user_input, int):
+			logger.debug(f"user_input: {user_input} is type 'int'")
+			logger.info(f"user_input: {user_input} has valid type")
+			
 			return user_input
 		
 		# user_input is str
 		elif isinstance(user_input, str):
-			
+			logger.debug(f"user_input: {user_input} is type 'str'")
+
 			# user_input is str and decimal; like '4', '2' or '1'
 			if user_input.isdecimal():
 				# casts str to int
+				logger.debug(f"user_input: {user_input} is 'decimal'")
+				logger.info(f"user_input: {user_input} has valid type")
+				
 				return int(user_input)
+		
 			else:
-				raise ValueError("User_input has to be decimal")
+				logger.warning(f"user_input: {user_input} is type 'str' but not decimal")
+				
+				raise ValueError("User_input has to be type 'str' and decimal")
 		
 		# user_input is float
 		elif isinstance(user_input, float):
 			# casts float to int
+			logger.debug(f"user_input: {user_input} is type 'float'")
+			logger.info(f"user_input: {user_input} has valid type")
+
 			return int(user_input)
 		
 		# if user_input is not int, str or float
 		else:
+			logger.warning(f"user_input: {user_input} is not valid data_type!")
+			
 			raise TypeError("Wrong Value for User_input")
-	
+			
 	except (ValueError, TypeError) as e:
+		logger.error(f"HTTPException; status_code:400, detail={e}")
+		
 		raise HTTPException(status_code=400, detail=e)
 
 
 def validate_user_prompt_id(user_prompt_id):
 	"""does user_prompt_id exist in database?"""
 	try:
+		logger.debug(f"check if given user_prompt_id: {user_prompt_id}, is in database")
+		logger.info(f"validate user_prompt_id: {user_prompt_id}")
+		
+		logger.debug(f"load all user_prompt_ids form database and check, if given user_prompt_id: {user_prompt_id}, is in all id´s")
 		all_prompt_ids = db_mngr.load_user_prompt_ids()
 		
 		# if user_prmopt_id is in id´s from database
 		if user_prompt_id in all_prompt_ids:
+			logger.info(f"user_prompt_id: {user_prompt_id}, is valid")
+			
 			return user_prompt_id
+		
 		else:
+			logger.warning(f"user_prompt_id: {user_prompt_id}, is not in database")
+			
 			raise ValueError("given user_prompt_id does not exist in database")
 	
 	except ValueError as e:
+		logger.error(f"HTTPEsception, status_code?404, detail={e}")
+		
 		raise HTTPException(status_code=404, detail=e)
